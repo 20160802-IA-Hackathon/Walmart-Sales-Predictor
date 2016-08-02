6f9f6a6b76e002921d660b782ea4dded838b3f56
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2016-08-02 15:19:07 -0400

    add default values to form

--- static/js/main.js
+++ static/js/main.js
@@ -11,14 +11,10 @@ $(function(){
       labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
       datasets: [{
           label: `Choose Data!`,
-          data: [0, 0, 0, 0],
+          data: [1, 1, 1, 1],
       }]
     }
   });
-
-  function randomNumber(){
-    return Math.random();
-  }
 
   function graphUpdate(e){
     e.preventDefault();
@@ -32,16 +28,28 @@ $(function(){
       unemp:    $children.eq(5).val(),
       holiday:  $children.eq(6).val()
     }
+
+    if (!data.size){
+      data.size = 10000
+    }
+    if (!data.temp){
+      data.temp = 60
+    }
+    if (!data.fuel){
+      data.fuel = 2.8
+    }
+    if (!data.cpi){
+      data.cpi = 220
+    }
+    if (!data.unemp){
+      data.unemp = 8
+    }
     
     $.getJSON('/result',data)
       .done(function(response){
         console.log(response);
-
-        let newData = response["weekly_results"].map(function(data){
-          return data * randomNumber();
-        })
         
-        myChart.config.data.datasets[0].data = newData;
+        myChart.config.data.datasets[0].data = response["weekly_results"];
         let month = $children.eq(0).find('option:selected').text();
         myChart.config.data.datasets[0].label = `${month} Predicted Sales`
         myChart.update()
